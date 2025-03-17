750982e0afc805194710460950d6d91436c955f3
Leonardo Reis <leonardoanterodosreis@hotmail.com>
2025-03-17 14:27:23 -0300

    Commit1

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 
+def sorteio_nome(nomes):
+    return random.choice(nomes)
+
+def sorteio_numero():
+    return random.randint(1, 10)
+
 print("Criando projeto")
 
 nomes = ["Ana", "Leo", "Kai", "Zoe", "Lia", "Tom", "Eva", "Max", "Ben", "Ray"]
 
-nome_sorteado = random.choice(nomes)
+nome_sorteado = sorteio_nome(nomes)
 print(f"O nome sorteado é: {nome_sorteado}")
 
-numero_sorteado = random.randint(1, 10)
+numero_sorteado = sorteio_numero()
 print(f"O número sorteado é: {numero_sorteado}")
-
