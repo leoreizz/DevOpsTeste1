08c3f874337077169f5eea9654a200f56df1aa98
Leonardo Reis <leonardoanterodosreis@hotmail.com>
2025-03-17 14:03:51 -0300

    Sorteio-de-nomes
    Codigo

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import random
 print("Criando projeto")
+
+nomes = ["Ana", "Leo", "Kai", "Zoe", "Lia", "Tom", "Eva", "Max", "Ben", "Ray"]
+
+nome_sorteado = random.choice(nomes)
+print(f"O nome sorteado é: {nome_sorteado}")
