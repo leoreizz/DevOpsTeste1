98280f7365264e4a33e3ae4255cce63d1126f169
Leonardo Reis <leonardoanterodosreis@hotmail.com>
2025-03-17 14:17:02 -0300

    atualizacaocodigo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 print("Criando projeto")
 
@@ -5,10 +6,6 @@ nomes = ["Ana", "Leo", "Kai", "Zoe", "Lia", "Tom", "Eva", "Max", "Ben", "Ray"]
 
 nome_sorteado = random.choice(nomes)
 print(f"O nome sorteado é: {nome_sorteado}")
-=======
-
-print("Criando projeto")
-
 
 numero_sorteado = random.randint(1, 10)
 print(f"O número sorteado é: {numero_sorteado}")
