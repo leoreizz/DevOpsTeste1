d4c5b64808b56179f809007a33cf01beffff15ad
Leonardo Reis <leonardoanterodosreis@hotmail.com>
2025-03-17 14:35:32 -0300

    commit5

--- main.py
+++ main.py
@@ -11,18 +11,21 @@ print("Criando projeto")
 nomes = ["Ana", "Leo", "Kai", "Zoe", "Lia", "Tom", "Eva", "Max", "Ben", "Ray"]
 
 nome_sorteado = sorteio_nome(nomes)
-print(f"O nome sorteado é: {nome_sorteado}")
 
 numero_sorteado = sorteio_numero()
-print(f"O número sorteado é: {numero_sorteado}")
 
 quantidade_sorteios = int(input("Quantos sorteios você quer realizar? "))
 
 print(f"Realizando {quantidade_sorteios} sorteios...")
 
+soma_numeros = 0
+
 for _ in range(quantidade_sorteios):
     nome_sorteado = sorteio_nome(nomes)
     numero_sorteado = sorteio_numero()
     print(f"O nome sorteado é: {nome_sorteado} e o número sorteado é: {numero_sorteado}")
+    soma_numeros += numero_sorteado
+
+print(f"A soma dos números sorteados é: {soma_numeros}")
 
 print("Todos os sorteios foram realizados com sucesso!")
